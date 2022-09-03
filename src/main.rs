d398fbdfe5bea5450c4d1bedb14794a066b51009
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-09-03 22:57:08 +0100

    refactor and test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use anyhow::{Context, Result};
+use clap::Parser;
+
+// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+// iterate over the lines and prine each one that contains provided pattern
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // load cli argumenents into CLI struct
+    let args = Cli::parse();
+
+    // open the file
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+// tests
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
